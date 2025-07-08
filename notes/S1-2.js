fcb5c0fcdae0ecedfce98ce087e1bc706ade22a7
revlienn <revnathalia@gmail.com>
2025-07-08 10:39:01 +0100

    S1

---
+++ notes/S1-2.js
@@ -0,0 +1,15 @@
+/*
+
+// SECTION 1
+
+git your project
+cmd, ng new project-name, css, no ssr, 
+    go to your dir, npm start
+
+delete app style and app.spec.ts
+app template
+    clear, except router-outlet
+app script
+    clear the style, cos we deleted it
+
+*/
