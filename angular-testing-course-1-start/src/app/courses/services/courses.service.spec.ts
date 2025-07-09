2fffa993d4ea62448b1aa0e7b1c78f77974492ae
revlienn <revnathalia@gmail.com>
2025-07-09 11:09:14 +0100

    13

--- angular-testing-course-1-start/src/app/courses/services/courses.service.spec.ts
+++ angular-testing-course-1-start/src/app/courses/services/courses.service.spec.ts
@@ -6,6 +6,7 @@ import {
   withInterceptorsFromDi,
 } from "@angular/common/http";
 import { provideHttpClientTesting } from "@angular/common/http/testing";
+import { COURSES } from "../../../../server/db-data";
 
 describe("CoursesService", () => {
   let coursesService: CoursesService,
@@ -25,6 +26,16 @@ describe("CoursesService", () => {
   });
 
   it("should retrieve all courses", () => {
-    pending();
+    coursesService.findAllCourses().subscribe((courses) => {
+      expect(courses).toBeTruthy();
+      expect(courses.length).toBe(12, "Error: incorrect number of courses");
+      const course12 = courses.find((c) => c.id == 12);
+      expect(course12.titles.description).toBe("Angular Testing Course");
+    });
+
+    const req = httpTestingController.expectOne("/api/courses");
+    expect(req.request.method).toBe("GET");
+
+    req.flush({ payload: Object.values(COURSES) });
   });
 });
--- notes/S2.js
+++ notes/S2.js
@@ -47,3 +47,38 @@ You’re now ready to:
     Assert the results and verify HTTP behavior
 
 */
+
+// COURSE 13
+/*
+
+want Test that CoursesService.findAllCourses():
+    Sends a GET request to /api/courses
+    Returns 12 course objects
+    Includes a course with ID 12 that has the title "Angular Testing Course"
+
+Step 1: Call the Method Under Test
+Execute and subscribe CoursesService.findAllCourses().subscribe
+
+Step 2: Assertions for the Response
+Expects:
+    Data exists (not null/undefined).
+    Exactly 12 courses are returned.
+    One of the courses has the expected title.
+        init var for the course12 first
+
+Step 3: Assertion the HTTP Request
+Expects:
+    One GET request was made to /api/courses.
+
+Step 4: Simulate the Backend Response
+Use the flush() method, feed mock data to subscribe() logic from Step 1.
+    req.flush({
+        payload: Object.values(CoURSES), // CoURSES is imported from your test data
+    });
+
+Step 5: Ensure Isolation and No Real Server Needed
+Cmd, you can stop the server, tests still run bcos we dont use real server, we mock them
+Confirm that no real HTTP requests are sent.
+All data is mocked and controlled inside the test.
+
+*/
